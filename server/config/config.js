3a369d0ac0fb731c08c6a55202f6f601598c1010
Maxi Gómez <56489609+maxiggomez@users.noreply.github.com>
2019-11-16 18:21:04 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
      urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    urlDB = 'mongodb+srv://admin:Y3v52JcVs5RgofsR@cluster0-4ys7c.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
